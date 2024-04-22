4fb27aad871baa98e46422a5cfe037b4aa9e19c4
akankshadeo <115640910+akankshadeo@users.noreply.github.com>
2024-04-22 22:58:10 +0530

    Update hpc6.cpp

--- hpc6.cpp
+++ hpc6.cpp
@@ -1,99 +1,74 @@
-vectorAdd.cu
-#include <cstdlib>
+Write a CUDA Program for :
+1. Addition of two large vectors
+2. Matrix Multiplication using CUDA C
+ Practical 4
+!nvcc --version
+nvcc: NVIDIA (R) Cuda compiler driver
+Copyright (c) 2005-2023 NVIDIA Corporation
+Built on Tue_Aug_15_22:02:13_PDT_2023
+Cuda compilation tools, release 12.2, V12.2.140
+Build cuda_12.2.r12.2/compiler.33191640_0
+!pip install git+https://github.com/afnan47/cuda.git
+%load_ext nvcc_plugin
+Collecting git+https://github.com/afnan47/cuda.git
+ Cloning https://github.com/afnan47/cuda.git to /tmp/pip-req-build-u12yi4kr
+ Running command git clone --filter=blob:none --quiet https://github.com/afnan47/cuda.git /tmp/pip
+ Resolved https://github.com/afnan47/cuda.git to commit aac710a35f52bb78ab34d2e52517237941399eff
+ Preparing metadata (setup.py) ... done
+directory /content/src already exists
+Out bin /content/result.out
+ Addition of two large vectors
+%%cu
 #include <iostream>
-#define checkCudaErrors(call)
-\
- do {
-\
- cudaError_t err = call;
-\
- if (err != cudaSuccess) {
-\
- print("CUDA error at %s %d: %s\n",
-__FILE__, __LINE__, cudaGetErrorString(err)); \
- exit(EXIT_FAILURE);
-\
- }
-\
- } while (0)
 using namespace std;
-// VectorAdd parallel funcƟon
-__global__ void vectorAdd(int *a, int *b, int
-*result, int n) {
- int Ɵd = threadIdx.x + blockIdx.x * blockDim.x;
- if (Ɵd < n) {
- result[Ɵd] = a[Ɵd] + b[Ɵd];
- }
+__global__ void add(int* A, int* B, int* C, int size) {
+int tid = blockIdx.x * blockDim.x + threadIdx.x;
+if (tid < size) {
+C[tid] = A[tid] + B[tid];
+}
+}
+void initialize(int* vector, int size) {
+for (int i = 0; i < size; i++) {
+vector[i] = rand() % 10;
+}
+}
+void print(int* vector, int size) {
+for (int i = 0; i < size; i++) {
+cout << vector[i] << " ";
+}
+cout << endl;
 }
 int main() {
- int *a, *b, *c;
- int *a_dev, *b_dev, *c_dev;
- int n = 1 << 4;
- a = new int[n];
- b = new int[n];
- c = new int[n];
- int *d = new int[n];
- int size = n * sizeof(int);
- checkCudaErrors(cudaMalloc(&a_dev, size));
- checkCudaErrors(cudaMalloc(&b_dev, size));
- checkCudaErrors(cudaMalloc(&c_dev, size));
- // Array iniƟalizaƟon..You can use Randon
-funcƟon to assign values
- for (int i = 0; i < n; i++) {
- a[i] = rand() % 1000;
- b[i] = rand() % 1000;
- d[i] = a[i] + b[i]; // calculaƟng serial addiƟon
- }
- cout << "Given array A is =>\n";
- for (int i = 0; i < n; i++) {
- cout << a[i] << ", ";
- }
- cout << "\n\n";
- cout << "Given array B is =>\n";
- for (int i = 0; i < n; i++) {
- cout << b[i] << ", ";
- }
- cout << "\n\n";
- cudaEvent_t start, end;
- checkCudaErrors(cudaEventCreate(&start));
- checkCudaErrors(cudaEventCreate(&end));
- checkCudaErrors(cudaMemcpy(a_dev, a, size,
-cudaMemcpyHostToDevice));
- checkCudaErrors(cudaMemcpy(b_dev, b, size,
-cudaMemcpyHostToDevice));
- int threads = 1024;
- int blocks = (n + threads - 1) / threads;
- checkCudaErrors(cudaEventRecord(start));
- // Parallel addiƟon program
- vectorAdd<<<blocks, threads>>>(a_dev, b_dev,
-c_dev, n);
- checkCudaErrors(cudaEventRecord(end));
- checkCudaErrors(cudaEventSynchronize(end));
- float Ɵme = 0.0;
- checkCudaErrors(cudaEventElapsedTime(&Ɵme,
-start, end));
- checkCudaErrors(cudaMemcpy(c, c_dev, size,
-cudaMemcpyDeviceToHost));
- // Calculate the error term.
- cout << "CPU sum is =>\n";
- for (int i = 0; i < n; i++) {
- cout << d[i] << ", ";
- }
- cout << "\n\n";
- cout << "GPU sum is =>\n";
- for (int i = 0; i < n; i++) {
- cout << c[i] << ", ";
- }
- cout << "\n\n";
- int error = 0;
- for (int i = 0; i < n; i++) {
- error += d[i] - c[i];
- if (0 != (d[i] - c[i])) {
- cout << "Error at (" << i << ") => GPU: " <<
-c[i] << ", CPU: " << d[i] << "\n";
- }
- }
- cout << "\nError : " << error;
- cout << "\nTime Elapsed: " << Ɵme;
- return 0;
+int N = 4;
+int* A, * B, * C;
+int vectorSize = N;
+size_t vectorBytes = vectorSize * sizeof(int);
+A = new int[vectorSize];
+B = new int[vectorSize];
+C = new int[vectorSize];
+initialize(A, vectorSize);
+initialize(B, vectorSize);
+cout << "Vector A: ";
+print(A, N);
+cout << "Vector B: ";
+print(B, N);
+int* X, * Y, * Z;
+cudaMalloc(&X, vectorBytes);
+cudaMalloc(&Y, vectorBytes);
+cudaMalloc(&Z, vectorBytes);
+cudaMemcpy(X, A, vectorBytes, cudaMemcpyHostToDevice);
+cudaMemcpy(Y, B, vectorBytes, cudaMemcpyHostToDevice);
+int threadsPerBlock = 256;
+int blocksPerGrid = (N + threadsPerBlock - 1) / threadsPerBlock;
+add<<<blocksPerGrid, threadsPerBlock>>>(X, Y, Z, N);
+cudaMemcpy(C, Z, vectorBytes, cudaMemcpyDeviceToHost);
+cout << "Addition: ";
+print(C, N);
+delete[] A;
+delete[] B;
+delete[] C;
+cudaFree(X);
+cudaFree(Y);
+cudaFree(Z);
+return 0;
 }
